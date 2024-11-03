8538aeb9dab1e391bfae9207925e991fb080cb51
Halokid <r00txxyyzz@gmail.com>
2024-11-04 00:18:36 +0800

    change service methond for consul

--- src/registry/consul.rs
+++ src/registry/consul.rs
@@ -24,7 +24,7 @@ impl Consul {
 impl RegClient for Consul {
 
   async fn get_service(&self, service_name: &str) -> Result<Vec<String>, CustomErr> {
-    let nodes = self.client.service_get(service_name.to_string()).await;
+    let nodes = self.client.service_getnodes(service_name.to_string()).await;
     Ok(nodes)
     // let vs = Vec::new();
     // Ok(vs)
